38de05dde3ef0448a56fa84edb7adb3d801b8ad3
xinyu.qin <qin.xinyu@zfax.com>
2019-01-19 13:51:48 +0800

    1

--- eruka-web/src/main/java/com/rtttx/eruka/core/ErukaWebApplication.java
+++ eruka-web/src/main/java/com/rtttx/eruka/core/ErukaWebApplication.java
@@ -13,8 +13,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ErukaApplication {
+public class ErukaWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ErukaApplication.class, args);
+        SpringApplication.run(ErukaWebApplication.class, args);
     }
 }
